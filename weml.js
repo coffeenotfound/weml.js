340f0d3eed296cc15ce923bde744d0b3c9ef00f5
coffeenotfound <coffeenotfound@users.noreply.github.com>
2017-01-15 08:50:24 +0100

    Add support for SIMD via selectTypeprototype()

--- weml.js
+++ weml.js
@@ -3,4 +3,9 @@ var weml = {
 	allocateArray: function(size) {
 		return new Float32Array(size);
 	},
+	
+	selectTypeprototype: function(clazz) {
+		const hasSIMD = window.SIMD !== undefined;
+		clazz.typeprototype._current = (hasSIMD ? clazz.typeprototype.simd : null) || clazz.typeprototype.sisd;
+	},
 };

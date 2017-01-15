2418a80930f896556d6ec3fc60d59d6ac50a6a86
coffeenotfound <coffeenotfound@users.noreply.github.com>
2017-01-15 08:55:55 +0100

    Add Mat4 class with SISD implementation

--- weml.js
+++ weml.js
@@ -503,3 +503,215 @@ Vec4.typeprototype = {};
 	// select typeprototype
 	weml.selectTypeprototype(Vec4);
 })();
+
+
+// ### class Mat4 ###
+var Mat4 = function(values) {
+	var mat = weml.allocateArray(16);
+	Object.assign(mat, Mat4.typeprototype._current);
+	if(values) mat.set(values);
+	return mat;
+};
+
+Mat4.typeprototype = {};
+(function() {
+// ## sisd implementation ##
+	Mat4.typeprototype.sisd = {
+		
+		// 00 10 20 30
+		// 01 11 21 31
+		// 02 12 22 32
+		// 03 13 23 33
+		//
+		// 00 04 08 12
+		// 01 05 09 13
+		// 02 06 10 14
+		// 03 07 11 15
+		//
+		// find:
+		// (?>(m00)|(m01)|(m02)|(m03)|(m10)|(m11)|(m12)|(m13)|(m20)|(m21)|(m22)|(m23)|(m30)|(m31)|(m32)|(m33))
+		// replace:
+		//	a(?1\[00\])(?2\[01\])(?3\[02\])(?4\[03\])(?5\[04\])(?6\[05\])(?7\[06\])(?8\[07\])(?9\[08\])(?10\[09\])(?11\[10\])(?12\[11\])(?13\[12\])(?14\[13\])(?15\[14\])(?16\[15\])
+		
+		toString: function() {
+			return "Mat4[" + this[0] + ", " + this[1] + "..." + this[14] + ", " + this[15] + "]";
+		},
+		
+		clone: function() {
+			return new Mat4(this);
+		},
+		
+		identity: function() {
+			this.fill(0);
+			this[0] = 1;
+			this[5] = 1;
+			this[10] = 1;
+			this[15] = 1;
+			return this;
+		},
+		zero: function() {
+			this.fill(0);
+			return this;
+		},
+		
+		set: function(a) {
+			this[00] = a[00];
+			this[01] = a[01];
+			this[02] = a[02];
+			this[03] = a[03];
+			this[04] = a[04];
+			this[05] = a[05];
+			this[06] = a[06];
+			this[07] = a[07];
+			this[08] = a[08];
+			this[09] = a[09];
+			this[10] = a[10];
+			this[11] = a[11];
+			this[12] = a[12];
+			this[13] = a[13];
+			this[14] = a[14];
+			this[15] = a[15];
+			return this;
+		},
+		
+		get: function(o) {
+			o.set(this);
+			return o;
+		},
+		put: function(a) {
+			a.set(this);
+			return this;
+		},
+		
+		mul: function(a, o) {
+			o = o || this;
+			var nm00 = this[00] * a[00] + this[04] * a[01] + this[08] * a[02] + this[12] * a[03];
+			var nm01 = this[01] * a[00] + this[05] * a[01] + this[09] * a[02] + this[13] * a[03];
+			var nm02 = this[02] * a[00] + this[06] * a[01] + this[10] * a[02] + this[14] * a[03];
+			var nm03 = this[03] * a[00] + this[07] * a[01] + this[11] * a[02] + this[15] * a[03];
+			var nm10 = this[00] * a[04] + this[04] * a[05] + this[08] * a[06] + this[12] * a[07];
+			var nm11 = this[01] * a[04] + this[05] * a[05] + this[09] * a[06] + this[13] * a[07];
+			var nm12 = this[02] * a[04] + this[06] * a[05] + this[10] * a[06] + this[14] * a[07];
+			var nm13 = this[03] * a[04] + this[07] * a[05] + this[11] * a[06] + this[15] * a[07];
+			var nm20 = this[00] * a[08] + this[04] * a[09] + this[08] * a[10] + this[12] * a[11];
+			var nm21 = this[01] * a[08] + this[05] * a[09] + this[09] * a[10] + this[13] * a[11];
+			var nm22 = this[02] * a[08] + this[06] * a[09] + this[10] * a[10] + this[14] * a[11];
+			var nm23 = this[03] * a[08] + this[07] * a[09] + this[11] * a[10] + this[15] * a[11];
+			var nm30 = this[00] * a[12] + this[04] * a[13] + this[08] * a[14] + this[12] * a[15];
+			var nm31 = this[01] * a[12] + this[05] * a[13] + this[09] * a[14] + this[13] * a[15];
+			var nm32 = this[02] * a[12] + this[06] * a[13] + this[10] * a[14] + this[14] * a[15];
+			var nm33 = this[03] * a[12] + this[07] * a[13] + this[11] * a[14] + this[15] * a[15];
+			o[00] = nm00;
+			o[01] = nm01;
+			o[02] = nm02;
+			o[03] = nm03;
+			o[04] = nm10;
+			o[05] = nm11;
+			o[06] = nm12;
+			o[07] = nm13;
+			o[08] = nm20;
+			o[09] = nm21;
+			o[10] = nm22;
+			o[11] = nm23;
+			o[12] = nm30;
+			o[13] = nm31;
+			o[14] = nm32;
+			o[15] = nm33;
+			return o;
+			
+			/*
+			o = o || this;
+			o[00] = this[00] * a[00];
+			o[01] = this[01] * a[01];
+			o[02] = this[02] * a[02];
+			o[03] = this[03] * a[03];
+			o[04] = this[04] * a[04];
+			o[05] = this[05] * a[05];
+			o[06] = this[06] * a[06];
+			o[07] = this[07] * a[07];
+			o[08] = this[08] * a[08];
+			o[09] = this[09] * a[09];
+			o[10] = this[10] * a[10];
+			o[11] = this[11] * a[11];
+			o[12] = this[12] * a[12];
+			o[13] = this[13] * a[13];
+			o[14] = this[14] * a[14];
+			o[15] = this[15] * a[15];
+			return o;
+			*/
+		},
+		
+		translate: function(a, o) {
+			o = o || this;
+			o[12] = this[00] * a[0] + this[04] * a[1] + this[08] * a[2] + this[12];
+			o[13] = this[01] * a[0] + this[05] * a[1] + this[09] * a[2] + this[13];
+			o[14] = this[02] * a[0] + this[06] * a[1] + this[10] * a[2] + this[14];
+			o[15] = this[03] * a[0] + this[07] * a[1] + this[11] * a[2] + this[15];
+			return o;
+		},
+		translateXYZ: function(x, y, z, o) {
+			o = o || this;
+			o[12] = this[00] * x + this[04] * y + this[08] * z + this[12];
+			o[13] = this[01] * x + this[05] * y + this[09] * z + this[13];
+			o[14] = this[02] * x + this[06] * y + this[10] * z + this[14];
+			o[15] = this[03] * x + this[07] * y + this[11] * z + this[15];
+			return o;
+		},
+		
+		perspective: function(fovy, aspect, near, far, o) {
+			o = o || this;
+			var h = Math.tan(fovy * 0.5);
+			
+			// calculate right matrix elements
+			var rm00 = 1.0 / (h * aspect);
+			var rm11 = 1.0 / h;
+			var rm22;
+			var rm32;
+			/*
+			boolean farInf = far > 0 && Float.isInfinite(far);
+			boolean nearInf = near > 0 && Float.isInfinite(near);
+			if (farInf) {
+				// See: "Infinite Projection Matrix" (http://www.terathon.com/gdc07_lengyel.pdf)
+				float e = 1E-6f;
+				rm22 = e - 1.0f;
+				rm32 = (e - (zZeroToOne ? 1.0f : 2.0f)) * near;
+			} else if (nearInf) {
+				float e = 1E-6f;
+				rm22 = (zZeroToOne ? 0.0f : 1.0f) - e;
+				rm32 = ((zZeroToOne ? 1.0f : 2.0f) - e) * far;
+			} else {
+			*/
+			const zZeroToOne = false;
+			rm22 = (zZeroToOne ? far : far + near) / (near - far);
+			rm32 = (zZeroToOne ? far : far + far) * near / (near - far);
+			/*
+			}
+			*/
+			// perform optimized matrix multiplication
+			var nm20 = this[08] * rm22 - this[12];
+			var nm21 = this[09] * rm22 - this[13];
+			var nm22 = this[10] * rm22 - this[14];
+			var nm23 = this[11] * rm22 - this[15];
+			o[00] = this[00] * rm00;
+			o[01] = this[01] * rm00;
+			o[02] = this[02] * rm00;
+			o[03] = this[03] * rm00;
+			o[04] = this[04] * rm11;
+			o[05] = this[05] * rm11;
+			o[06] = this[06] * rm11;
+			o[07] = this[07] * rm11;
+			o[12] = this[08] * rm32;
+			o[13] = this[09] * rm32;
+			o[14] = this[10] * rm32;
+			o[15] = this[11] * rm32;
+			o[08] = nm20;
+			o[09] = nm21;
+			o[10] = nm22;
+			o[11] = nm23;
+			return o;
+		},
+	};
+	
+	// select typeprototype
+	weml.selectTypeprototype(Mat4);
+})();
